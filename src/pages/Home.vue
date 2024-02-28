2c0e0e6d6b63a7a98a6ee52d407e4d9af87c8967
Dmitry Bystrov <dmit.bystroff2016@yandex.ru>
2024-02-28 17:32:08 +0300

    feat:  made a button component, connected fonts and set up the main page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,6 +1,32 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Button } from '@/shared/button';
+</script>
+
 <template>
-  <main><div>Home</div></main>
+  <main>
+    <div class="main">
+      <h1 class="main__title">TODOS</h1>
+      <div class="main__button"><Button>К списку дел</Button></div>
+    </div>
+  </main>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+.main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+  min-height: 100vh;
+
+  &__title {
+    color: darkorchid;
+    font-weight: 900;
+  }
+
+  &__button {
+    width: 200px;
+  }
+}
+</style>
---
+++ src/shared/button/ui.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts"></script>
+
+<template>
+  <button class="button">
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped lang="scss">
+.button {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 0.5rem;
+  width: 100%;
+  background-color: transparent;
+  font-size: 1.4rem;
+  background: linear-gradient(white, white) padding-box,
+    linear-gradient(to right, darkblue, darkorchid) border-box;
+  border-radius: 0.5rem;
+  border: 4px solid transparent;
+  padding: 0.5rem;
+  cursor: pointer;
+  transition: all 0.3s;
+
+  &:hover {
+    transform: scale(1.1);
+  }
+}
+</style>
